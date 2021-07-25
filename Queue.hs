ad86b032ae5794ab6631eddb156d5884f99a0b48
jrvieira <github@jrvieira.com>
2021-07-25 21:19:58 +0100

    Zero.Queue stable

--- Queue.hs
+++ Queue.hs
@@ -1,11 +1,12 @@
 module Zero.Queue where
 
+import Data.Foldable
 
 -- queue
 data Queue a = Queue [a] [a]
 
 instance Semigroup (Queue a) where
-   Queue ia oa <> Queue ib ob = Queue (ib <> reverse ob) (oa <> reverse ia)
+   Queue ia oa <> Queue ib ob = Queue ib (oa <> reverse ia <> ob)
 
 instance Monoid (Queue a) where
    mempty = Queue [] []
@@ -14,13 +15,20 @@ instance Functor Queue where
    fmap f (Queue i o) = Queue (fmap f i) (fmap f o)
 
 instance Foldable Queue where
-   foldr f z (Queue i o) = foldr f (foldr f z o) (reverse i)
+   foldr f z (Queue i o) = foldr f (foldr f z $ reverse i) o
+
+instance Show a => Show (Queue a) where
+   show q = unwords ["queue",show $ toList q]
 
 queue :: [a] -> Queue a
 queue = Queue []
 
 enqueue :: a -> Queue a -> Queue a
 enqueue x (Queue i o) = Queue (x:i) o
+
+infixr 7 >|
+(>|) :: a -> Queue a -> Queue a
+(>|) = enqueue
 
 dequeue :: Queue a -> (Maybe a,Queue a)
 dequeue q@(Queue i o)
