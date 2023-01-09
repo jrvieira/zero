98db41a4f11171f5f340b213d967fe2dd7e08316
jrvieira <github@jrvieira.com>
2023-01-09 18:04:17 +0000

    clean up

--- Echo.hs
+++ Echo.hs
@@ -8,7 +8,7 @@ import Control.Arrow ( (&&&) )
 import Control.Monad ( join )
 
 echo :: [((Int,Int),Char)] -> IO ()
-echo l = putStrLn $ unlines $ chunksOf (2 * (succ $ xω - xα)) $ intersperse ' ' $ cell <$> [ (x,y) | y <- [yα..yω] , x <- [xα..xω] ]
+echo l = putStrLn $ unlines $ chunksOf (2 * succ (xω - xα)) $ intersperse ' ' $ cell <$> [ (x,y) | y <- [yα..yω] , x <- [xα..xω] ]
    where
    cell = maybe ' ' id . (m !?)
    ((xα,xω),(yα,yω)) = join bimap (minimum &&& maximum) $ unzip $ keys m
